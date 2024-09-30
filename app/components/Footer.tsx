d2939a3713a3d9737c35a7a151a5fb635c0a10b3
dev.maheshsuthar <dev.maheshsuthar@gmail.com>
2024-09-30 11:14:37 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -19,7 +19,7 @@ export const Footer = async () => {
       <div className="flex justify-center">
         <div className="bigtab:flex-nowrap] relative z-[2] flex flex-wrap gap-y-[30px]  pt-[40px] sm:justify-between  lg:pt-[80px] small:gap-y-[10px] 3xl:max-w-[1200px]">
           <div className="order-1 w-[100%] max-w-full bigtab:mr-[6%] bigtab:w-[40%]">
-            <div className=" mr-auto text-[20px] font-[400] leading-[164%] sm:text-[25px] xl:max-w-[80%]">
+            <div className=" gotham mr-auto text-[20px] font-[400] font-[400] leading-[164%] sm:text-[25px] xl:max-w-[80%]">
               A World of Leading Expertise Exclusively yours
             </div>
             <div className=" mr-auto mt-[5px] text-[14px] font-[500] leading-[147%] sm:text-[25px] xl:mt-[38px] xl:max-w-[90%]">
@@ -28,11 +28,11 @@ export const Footer = async () => {
             </div>
             <FooterEmailForm />
           </div>
-          <div className="order-2 w-full font-[200] sm:w-[calc(30%-20px)] sm:text-[16px] xl:w-[18%] bigtab:w-[15%]">
+          <div className="helveticaNeue order-2 w-full font-[200] sm:w-[calc(30%-20px)] sm:text-[16px] xl:w-[18%] bigtab:w-[15%]">
             <div className="font-[700]">EXPLORE RESIDENTIAL IN</div>
             <a
-              href="/projects/ready?availablefor=buy&construction_status=ready&location=Palm%20Jumeirah"
-              className={`mt-[7px] block w-fit ${s.hoverable}`}
+              href="/projects/ready?availablefor=buy&construction_status=ready&location=Palm%20Jumeirah "
+              className={`mt-[7px] block w-fit ${s.hoverable} `}
             >
               Palm Jumeirah
             </a>
