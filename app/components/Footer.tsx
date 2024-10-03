c66f4f1e5e5b8d8b951753ee5525bf50e2fdc3b0
dev.maheshsuthar <dev.maheshsuthar@gmail.com>
2024-10-03 11:53:19 +0530

    commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = async () => {
 
   return (
     <footer
-      className={`min-[1440px]-h-[477px] relative bg-cover  bg-right-bottom sm:bg-top `}
+      className={`min-[1440px]-h-[477px] relative mt-[25px]  bg-cover bg-right-bottom sm:bg-top`}
       style={{ backgroundImage: 'url("/images/ft-bg.jpg")' }}
     >
       <div className={`z-[1] ${styles.tint} z-[1]`}></div>
